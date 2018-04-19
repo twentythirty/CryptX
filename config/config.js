ba2168b13e96243192ee0ded3cde6d261aa8a001
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-04-19 13:18:06 +0300

    fix logger format

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ CONFIG.port = process.env.PORT || '3000';
 CONFIG.db_dialect = process.env.DB_DIALECT || 'postgres';
 CONFIG.DATABASE_URL = process.env.DATABASE_URL
 CONFIG.disclaimer = process.env.disclaimer || 'Parcel Pending API';
-CONFIG.logger_format = process.env.logger_format || ':remote-addr [:date[clf]] :method :url :status :response-time ms - :res[content-length]';
+CONFIG.logger_format = process.env.LOGGER_FORMAT || ':remote-addr [:date] :method :url :status :response-time ms - :res[content-length]';
 
 CONFIG.jwt_encryption = process.env.JWT_ENCRYPTION;
 CONFIG.jwt_expiration = process.env.JWT_EXPIRATION;
