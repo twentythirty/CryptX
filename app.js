dcc0ce0bebf84cabae0cede0fd7ef7a0fe8fd5bd
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-04-20 10:28:15 +0300

    Add new system source

---
+++ app.js
@@ -0,0 +1,87 @@
+require('./config/config');     //instantiate configuration variables
+require('./global_functions');  //instantiate global functions
+
+console.log("App Environment:", CONFIG.app)
+console.log("Resolved NODE_ENV: ", process.env.NODE_ENV)
+
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const v1 = require('./routes/v1');
+
+const app = express();
+
+logger.token('date', function () {
+    return new Date().toString()
+})
+
+app.use(logger(CONFIG.logger_format));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+//Passport
+app.use(passport.initialize());
+
+//DATABASE
+const models = require("./models");
+models.sequelize.authenticate().then(() => {
+    console.log('Connected to SQL database:', process.env.DATABASE_URL);
+})
+    .catch(err => {
+        console.error('Unable to connect to SQL database:', process.env.DATABASE_URL, err);
+        process.exit(2);
+    });
+if (CONFIG.app === 'dev') {
+    models.sequelize.sync();//creates table if they do not already exist
+}
+//sync migrations
+require('./migrator');
+console.log('Performing startup migration...');
+migratorPerform();
+
+
+// CORS
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, Content-Type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use('/v1', v1);
+
+app.use('/', function (req, res) {
+    res.statusCode = 200;//send the appropriate status code
+    res.json({ status: "success", message: CONFIG.disclaimer, data: {} })
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ migrator.js
@@ -0,0 +1,150 @@
+require('./config/config');     //instantiate configuration variables
+//DATABASE
+const models = require("./models");
+const path = require('path');
+
+var Umzug = require('umzug');
+var umzug = new Umzug({
+    storage: 'sequelize',
+    storageOptions: {
+        sequelize: models.sequelize
+    },
+    migrations: {
+        params: [
+            models.sequelize.getQueryInterface(), // queryInterface
+            models.sequelize.constructor, // DataTypes
+            function () {
+                throw new Error('Migration tried to use old style "done" callback. Please upgrade to "umzug" and return a promise instead.');
+            }
+        ],
+        // The path to the migrations directory.
+        path: 'migrations',
+    }
+});
+
+function cmdStatus() {
+    let result = {};
+
+    return umzug.executed()
+        .then(executed => {
+            result.executed = executed;
+            return umzug.pending();
+        }).then(pending => {
+            result.pending = pending;
+            return result;
+        }).then(({ executed, pending }) => {
+
+            executed = executed.map(m => {
+                m.name = path.basename(m.file, '.js');
+                return m;
+            });
+            pending = pending.map(m => {
+                m.name = path.basename(m.file, '.js');
+                return m;
+            });
+
+            const current = executed.length > 0 ? executed[0].file : '<NO_MIGRATIONS>';
+            const status = {
+                current: current,
+                executed: executed.map(m => m.file),
+                pending: pending.map(m => m.file),
+            }
+
+            console.log(JSON.stringify(status, null, 2))
+
+            return { executed, pending };
+        })
+}
+
+function cmdMigrate() {
+    return umzug.up();
+}
+
+function cmdMigrateNext() {
+    return cmdStatus()
+        .then(({ executed, pending }) => {
+            if (pending.length === 0) {
+                return Promise.reject(new Error('No pending migrations'));
+            }
+            const next = pending[0].name;
+            return umzug.up({ to: next });
+        })
+}
+
+function cmdReset() {
+    return umzug.down({ to: 0 });
+}
+
+function cmdResetPrev() {
+    return cmdStatus()
+        .then(({ executed, pending }) => {
+            if (executed.length === 0) {
+                return Promise.reject(new Error('Already at initial state'));
+            }
+            const prev = executed[executed.length - 1].name;
+            return umzug.down({ to: prev });
+        })
+}
+
+
+let runningMigrator = process.argv[1].split(/\//).pop() === 'migrator.js';
+
+if (runningMigrator) {
+
+    const cmd = process.argv[2].trim();
+    let executedCmd;
+
+    console.log(`${cmd.toUpperCase()} BEGIN`);
+    switch (cmd) {
+        case 'status':
+            executedCmd = cmdStatus();
+            break;
+
+        case 'up':
+        case 'migrate':
+            executedCmd = cmdMigrate();
+            break;
+
+        case 'next':
+        case 'migrate-next':
+            executedCmd = cmdMigrateNext();
+            break;
+
+        case 'down':
+        case 'reset':
+            executedCmd = cmdReset();
+            break;
+
+        case 'prev':
+        case 'reset-prev':
+            executedCmd = cmdResetPrev();
+            break;
+
+        default:
+            console.log(`invalid cmd: ${cmd}`);
+            process.exit(1);
+    }
+
+    executedCmd
+        .then((result) => {
+            const doneStr = `${cmd.toUpperCase()} DONE`;
+            console.log(doneStr);
+            console.log("=".repeat(doneStr.length));
+        })
+        .catch(err => {
+            const errorStr = `${cmd.toUpperCase()} ERROR`;
+            console.log(errorStr);
+            console.log("=".repeat(errorStr.length));
+            console.log(err);
+            console.log("=".repeat(errorStr.length));
+        })
+        .then(() => {
+            if (cmd !== 'status' && cmd !== 'reset-hard') {
+                return cmdStatus()
+            }
+            return Promise.resolve();
+        })
+        .then(() => process.exit(0))
+}
+
+migratorPerform = cmdMigrate;
