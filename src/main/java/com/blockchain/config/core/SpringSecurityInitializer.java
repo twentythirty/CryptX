238cf4a321d6ae1d606a529246530e58b9b2ddad
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-04-20 10:25:58 +0300

    Remove old system in master

--- src/main/java/com/blockchain/config/core/SpringSecurityInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.blockchain.config.core;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer
-  extends AbstractSecurityWebApplicationInitializer
-{
-  public SpringSecurityInitializer() {}
-}
