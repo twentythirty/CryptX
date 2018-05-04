7ef8ed785f4fbe0fc83e93cea57cd4b0707c40fd
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-05-04 13:30:01 +0300

    populate instruments and supplementary ids table

--- migrations/20180426140211-populate-instruments.js
+++ migrations/20180426140211-populate-instruments.js
@@ -1,12 +1,45 @@
 "use strict";
 
 const req_prom = require("request-promise");
+const Instrument = require('../models').Instrument;
+const InstrumentCoinMarketCapInfo = require('../models').InstrumentCoinMarketCapInfo;
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    
+    return req_prom({
+        uri: "https://api.coinmarketcap.com/v2/listings/",
+        headers: {
+          "User-Agent": "Request-Promise"
+        },
+        json: true
+      })
+      .then(resp => {
+        const data = resp.data;
+        console.log(
+          "Got %s coins in response!",
+          resp.metadata.num_cryptocurrencies
+        );
+
+        //individual coin insert with supporting object save
+        return Promise.all(data.map(coin_data => {
+          return Instrument.create({
+            symbol: coin_data.symbol,
+            long_name: coin_data.name,
+            is_base: (coin_data.symbol === 'BTC' || coin_data.symbol === 'ETH'),
+            is_blacklisted: false,
+            tick_size: 0.0001
+          }).then(instrument => {
+            return InstrumentCoinMarketCapInfo.create({
+                instrument_id: instrument.id,
+                coinmarketcap_id: coin_data.id
+              });
+          });
+        }));
+
+      });
   },
   down: (queryInterface, Sequelize) => {
-    
+
+    return queryInterface.bulkDelete('instrument', {})
   }
 };
