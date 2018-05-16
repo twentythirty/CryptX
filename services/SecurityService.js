8079191f75f349c3282ac9d570fcd9429d1c7c9c
Tautvydasd Petkunas <tautvydas@mediapark.com>
2018-05-16 16:32:21 +0300

    modified changeRolePermissions to be used to edit roles in general tests for new methods

--- services/SecurityService.js
+++ services/SecurityService.js
@@ -6,43 +6,51 @@ const Permission = require('../models').Permission;
 
 const createRole = async function (role_name) {
 
-	if(role_name === null) TE("Role name can't be empty");
+	if(role_name == null) TE("Role name can't be empty");
 
-	let err, role;
-	role = new Role({
-			name: role_name
-	});
+	let err, role, new_role = {
+		name: role_name
+	};
 
-	[err, role] = await to(role.save());
+	[err, role] = await to(Role.create(new_role));
+
 	if (err) TE(err.message);
 
 	return role;
 };
 module.exports.createRole = createRole;
 
-const changeRolePermissions = async function (role_id, new_perms) {
-	let role = await Role.findById(role_id, {
+const editRole = async function (role_id, updated_role) {
+	let err, role = await Role.findById(role_id, {
 		include: [Permission]
 	});
 	if (!role) TE('Role with id %s not found!', role_id);
 
-	let err, neededPerms;
-	[err, neededPerms] = await to(Permission.findAll({
-		where: {
-			code: new_perms
-		}
-	}));
+	// update name if set
+	role.name = updated_role.name != null ? updated_role.name : role.name;
 
+	[err, role] = await to(role.save());
 	if (err) TE(err.message);
 
-	let assoc;
-	[err, assoc] = await to(role.setPermissions(neededPerms)); // waits until role and permission associations are written into DB.
+	if(updated_role.permissions != null) {
+		let neededPerms;
+		[err, neededPerms] = await to(Permission.findAll({
+			where: {
+				code: updated_role.permissions
+			}
+		}));
 
-	if (err) TE(err.message);
+		if (err) TE(err.message);
+
+		let assoc;
+		[err, assoc] = await to(role.setPermissions(neededPerms)); // waits until role and permission associations are written into DB.
+		
+		if (err) TE(err.message);
+	}
 
 	return role;
 }
-module.exports.changeRolePermissions = changeRolePermissions;
+module.exports.editRole = editRole;
 
 const deleteRole = async function (role_id) {
 
