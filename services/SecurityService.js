86ed555051ac7bd7e2828d3ab062ea2f9a80b794
Tautvydasd Petkunas <tautvydas@mediapark.com>
2018-05-18 14:18:17 +0300

    added feed back for when expecting to add permission thats missing in DB

--- services/SecurityService.js
+++ services/SecurityService.js
@@ -42,6 +42,12 @@ const editRole = async function (role_id, updated_role) {
 
 		if (err) TE(err.message);
 
+		// check if all permissions were found
+		let missingPerms = _.difference(
+			updated_role.permissions, neededPerms.map(perm => perm.code)
+		);
+		if (missingPerms.length) TE("Couldn't find permissions: %s", missingPerms);
+		
 		let assoc;
 		[err, assoc] = await to(role.setPermissions(neededPerms)); // waits until role and permission associations are written into DB.
 		
