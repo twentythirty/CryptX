1d1d83454c24429b3d269c1f262c145c6d186bb5
Edmundas <edmundas.riskus@gmail.com>
2018-07-12 10:33:19 +0300

    Documentation

--- front-end/src/app/modules/investment/investment-run-detail/investment-run-detail.component.ts
+++ front-end/src/app/modules/investment/investment-run-detail/investment-run-detail.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { TimelineDetailComponent, SingleTableDataSource } from '../timeline-detail/timeline-detail.component'
 import { ActivatedRoute } from '@angular/router';
+
+import { TimelineDetailComponent, SingleTableDataSource } from '../timeline-detail/timeline-detail.component'
 import { TableDataSource, TableDataColumn } from '../../../shared/components/data-table/data-table.component';
 import { TimelineEvent } from '../timeline/timeline.component';
 
+/**
+ * 0. Set HTML and SCSS files in component decorator
+ */
 @Component({
   selector: 'app-investment-run-detail',
   templateUrl: '../timeline-detail/timeline-detail.component.html',
@@ -11,11 +15,17 @@ import { TimelineEvent } from '../timeline/timeline.component';
 })
 export class InvestmentRunDetailComponent extends TimelineDetailComponent implements OnInit {
 
+  /**
+   * 1. Implement abstract attributes to display titles
+   */
   public pageTitle: string = 'Recipe run';
   public singleTitle: string = 'Investment run';
   public listTitle: string = 'Recipe runs';
   public addTitle: string = '+ Start new run';
 
+  /**
+   * 2. Implement abstract attributes to preset data structure
+   */
   public timelineEvents: Array<TimelineEvent>;
 
   public listColumnsToShow: Array<string | TableDataColumn> = [
@@ -30,7 +40,7 @@ export class InvestmentRunDetailComponent extends TimelineDetailComponent implem
       { column: 'two', name: 'Two', filter: { type: 'text', sortable: true }},
       { column: 'three', name: 'Three', filter: { type: 'text', sortable: true }}
     ],
-    body: null
+    body: null,
   };
 
   public singleDataSource: SingleTableDataSource = {
@@ -43,12 +53,19 @@ export class InvestmentRunDetailComponent extends TimelineDetailComponent implem
     body: null
   }
 
+  /**
+   * 3. Call super() with ActivatedRoute
+   * @param route - ActivatedRoute, used in DataTableCommonManagerComponent
+   */
   constructor(
     public route: ActivatedRoute
   ) {
     super(route);
   }
 
+  /**
+   * 4. Implement abstract methods to fetch data OnInit
+   */
   public getAllData(): void {
     this.listDataSource.body = [
       { one: 1, two: 2, three: 3 },
@@ -58,11 +75,26 @@ export class InvestmentRunDetailComponent extends TimelineDetailComponent implem
     this.count = 3;
   }
 
-  public getSingleData(): void {
+  protected getSingleData(): void {
     this.singleDataSource.body = [
       { one: 1, two: 2, three: 3 }
     ]
   }
+
+  protected getTimelineData(): void {
+    this.timelineEvents = Array(5).fill(
+      new TimelineEvent(
+        'Investment run',
+        'Orders filled',
+        'IR-001, rci',
+        '21 May, 2018 10:30'
+      )
+    )
+  }
+
+  /**
+   * 5. Implement abstract methods to handle user actions
+   */
 
   public addAction(): void {
     alert('add?')
@@ -76,16 +108,13 @@ export class InvestmentRunDetailComponent extends TimelineDetailComponent implem
     alert('Navigate to a row item page');
   }
 
+  /**
+   * + If custom ngOnInit() is needed, call super.ngOnInit() to
+   * perform parent component class initialization
+   */
+
   ngOnInit() {
     super.ngOnInit();
-    this.timelineEvents = Array(5).fill(
-      new TimelineEvent(
-        'Investment run',
-        'Orders filled',
-        'IR-001, rci',
-        '21 May, 2018 10:30'
-      )
-    )
   }
 
 }
