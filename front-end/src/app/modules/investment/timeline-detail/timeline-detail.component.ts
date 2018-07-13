99d0af9c0350ddf67f5ba1be1909e9617d636dcc
Edmundas <edmundas.riskus@gmail.com>
2018-07-13 16:28:23 +0300

    Footer

--- front-end/src/app/modules/investment/timeline-detail/timeline-detail.component.ts
+++ front-end/src/app/modules/investment/timeline-detail/timeline-detail.component.ts
@@ -127,4 +127,13 @@ export abstract class TimelineDetailComponent extends DataTableCommonManagerComp
     if(item.action) item.action();
   }
 
+  public setListFooter(res): void {
+    if(res.footer) {
+      this.listDataSource.footer = this.listColumnsToShow.map(col => {
+        let key = (typeof col == 'string') ? col : col.column;
+        return (res.footer.find(f => f.name == key) || {}).value || '';
+      })
+    }
+  }
+
 }
