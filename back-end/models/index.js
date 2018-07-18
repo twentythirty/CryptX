d8e0ab2cb9aeacf93410a075d0e9c9e69d4a4992
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-07-18 12:31:16 +0300

    add ENV-based SSL connection to DB

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -9,7 +9,7 @@ var db        = {};
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: CONFIG.db_dialect,
   dialectOptions: {
-    ssl: true
+    ssl: ['test', 'production'].includes(process.env.NODE_ENV)
   },
   operatorsAliases: false
 });
