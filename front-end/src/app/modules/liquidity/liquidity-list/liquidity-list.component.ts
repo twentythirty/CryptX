50d3cfa3d5b5f811a6ff66508b0caca2d3af8585
Oleg Granevskij <oleg.granevskij@mediapark.com>
2018-07-26 16:10:20 +0300

    Implemented liquidity requirements list

--- front-end/src/app/modules/liquidity/liquidity-list/liquidity-list.component.ts
+++ front-end/src/app/modules/liquidity/liquidity-list/liquidity-list.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { TableDataSource, TableDataColumn } from '../../../shared/components/data-table/data-table.component';
+import { DataTableCommonManagerComponent } from '../../../shared/components/data-table-common-manager/data-table-common-manager.component';
+import { NumberCellDataColumn } from '../../../shared/components/data-table-cells';
+import { LiquidityRequirement } from '../../../shared/models/liquidityRequirement';
+
+import { LiquidityService } from '../../../services/liquidity/liquidity.service';
 
 @Component({
   selector: 'app-liquidity-list',
   templateUrl: './liquidity-list.component.html',
   styleUrls: ['./liquidity-list.component.scss']
 })
-export class LiquidityListComponent implements OnInit {
+export class LiquidityListComponent extends DataTableCommonManagerComponent implements OnInit {
+  public liquidityDataSource: TableDataSource = {
+    header: [
+      { column: 'instrument', nameKey: 'table.header.instrument', filter: { type: 'text', sortable: true } },
+      { column: 'periodicity', nameKey: 'table.header.periodicity', filter: { type: 'number', sortable: true } },
+      { column: 'quote_asset', nameKey: 'table.header.quote_asset', filter: { type: 'text', sortable: true } },
+      { column: 'minimum_circulation', nameKey: 'table.header.minimum_circulation', filter: { type: 'number', sortable: true } },
+      { column: 'exchange', nameKey: 'table.header.exchange', filter: { type: 'text', sortable: true } },
+      { column: 'exchange_count', nameKey: 'table.header.exchange_count', filter: { type: 'number', sortable: true } },
+      { column: 'exchange_not_pass', nameKey: 'table.header.exchange_not_pass', filter: { type: 'number', sortable: true } },
+    ],
+    body: null,
+    footer: null,
+  };
 
-  constructor() { }
+  public liquidityColumnsToShow: Array<TableDataColumn> = [
+    new TableDataColumn({ column: 'instrument' }),
+    new TableDataColumn({ column: 'periodicity' }),
+    new TableDataColumn({ column: 'quote_asset' }),
+    new NumberCellDataColumn({ column: 'minimum_circulation' }),
+    new TableDataColumn({ column: 'exchange_count' }),
+    new TableDataColumn({ column: 'exchange' }),
+    new TableDataColumn({ column: 'exchange_not_pass' }),
+  ];
 
-  ngOnInit() {
+  constructor(
+    public route: ActivatedRoute,
+    private liquidityService: LiquidityService,
+  ) {
+    super(route);
+    this.getFilterLOV();
+  }
+
+  /**
+   * Add a rowData$ Observable to text and boolean column filters
+  */
+  private getFilterLOV(): void {
+    this.liquidityDataSource.header.filter(
+      col => ['instrument', 'quote_asset', 'exchange'].includes(col.column)
+    ).map(
+      col => {
+        col.filter.rowData$ = this.liquidityService.getHeaderLOV(col.column);
+      }
+    )
+  }
+
+  
+  getAllData(): void {
+    this.liquidityService.getAllLiquidities(this.requestData).subscribe(
+      res => {
+        Object.assign(this.liquidityDataSource, {
+          body: res.liquidity_requirements,
+          footer: res.footer
+        });
+        this.count = res.count;
+      }
+    );
+  }
+
+  openRow(liquidity: LiquidityRequirement): void {
+    console.log('liquidity', liquidity);
   }
 
 }
--- front-end/src/app/services/liquidity/liquidity.service.ts
+++ front-end/src/app/services/liquidity/liquidity.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
 
 import { EntitiesFilter } from '../../shared/models/api/entitiesFilter';
 import { environment } from '../../../environments/environment';
 import { LiquidityRequirementsCreateRequestData } from '../../shared/models/api/liquidityRequirementsCreateRequestData';
+import { LiquidityRequirement } from '../../shared/models/liquidityRequirement';
 
 export class LiquidityRequirementsCreateResponse {
   success: boolean;
   liquidity_requirement: any;
   error: string;
 }
+
+export class LiquiditiesAllResponse {
+  success: boolean;
+  count: number;
+  footer: Array<any>
+  liquidity_requirements: Array<LiquidityRequirement>;
+}
+
 
 @Injectable()
 export class LiquidityService {
@@ -20,8 +30,31 @@ export class LiquidityService {
     private http: HttpClient,
   ) {}
 
+  getAllLiquidities(request?: EntitiesFilter): Observable<LiquiditiesAllResponse>{
+    if (request) {
+      return this.http.post<LiquiditiesAllResponse>(this.baseUrl + `liquidity_requirements/all`, request);
+    } else {
+      return this.http.get<LiquiditiesAllResponse>(this.baseUrl + `liquidity_requirements/all`);
+    }
+  }
+
   createLiquidityRequirement(request: LiquidityRequirementsCreateRequestData): Observable<LiquidityRequirementsCreateResponse> {
-    return this.http.post<LiquidityRequirementsCreateResponse>(this.baseUrl + `/liquidity_requirements/create`, request);
+    return this.http.post<LiquidityRequirementsCreateResponse>(this.baseUrl + `liquidity_requirements/create`, request);
+  }
+
+  getHeaderLOV(column_name: string): Observable<any> {
+    return this.http.get<any>(this.baseUrl + `liquidity_requirements/header_lov/${column_name}`).pipe(
+      map(
+        res => {
+          if(res && res.lov && Array.isArray(res.lov)) {
+            return res.lov.map(lov => {
+              return { value: lov }
+            });
+          }
+          return null;
+        }
+      )
+    );
   }
 
 }
