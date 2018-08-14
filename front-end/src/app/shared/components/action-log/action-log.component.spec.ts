d4f4132eec0325765b7f6dd7598d9282a21c092c
Oleg Granevskij <oleg.granevskij@mediapark.com>
2018-08-14 13:29:42 +0300

    Created actions log component, added action log to execution orders fill

---
+++ front-end/src/app/shared/components/action-log/action-log.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ActionLogComponent } from './action-log.component';
+
+describe('ActionLogComponent', () => {
+  let component: ActionLogComponent;
+  let fixture: ComponentFixture<ActionLogComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ActionLogComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ActionLogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
