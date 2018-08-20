5dc3f15935e8bd9d12e7d0f4b4efc780a092305a
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-08-20 11:17:17 +0300

    jwt-srt fix

--- back-end/middleware/add_new_token_response.js
+++ back-end/middleware/add_new_token_response.js
@@ -5,14 +5,16 @@ const logger = require('../utils/ActionLogUtil');
 module.exports.response_token_refresh = (req, res, next) => {
 
     if (req.user == null || req.user.session == null) {
+        const message = `Missing user object or session object on request body for ${req.path}! Not adding new token to response...`;
+        console.error(`Missing user object or session object on request body for ${req.path}! Not adding new token to response...`);
         //leave this promise to persiste log messsage
-        logger.log(`Missing user object or session object on request body! Not adding new token to response...`, {
+        logger.log(message, {
             log_level: LOG_LEVELS.Error
         })
         next()
     }
     //add token string to response object (will be used in ReS global)
-    res.new_token = req.user.session.token;
+    res.next_token = req.user.session.token;
 
     next();
 };
