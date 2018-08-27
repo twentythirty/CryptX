a555b010618331eae29834e0dd8749357c99f70a
Tautvydas Petkunas <tautvydas@mediapark.com>
2018-08-27 18:08:38 +0300

    indexes for status and approval status columns in investment run workflow tables

---
+++ back-end/migrations/20180827173000-add-approval_status-index-recipe_order_group.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addIndex('recipe_order_group', {
+      fields: ['approval_status'],
+      name: 'recipe_order_group_approval_status'
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.addIndex('recipe_order_group', 'recipe_order_group_approval_status'); 
+  }
+};
