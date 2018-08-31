a4ecf5cc2732e0ebeb6bc68925a9ec1e704b4da2
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-08-31 17:34:46 +0300

    fix test reporter issues and add test runner

--- back-end/test-harness/heroku-mailing-reporter.js
+++ back-end/test-harness/heroku-mailing-reporter.js
@@ -1,11 +1,11 @@
 'use strict';
-var mocha = require('mocha');
 module.exports = MailingReporter;
 
 /**
  * Mocha tests reporter designed with Heroku builds in mind. 
  * 
  * Collects failing tests info and generates email for those who need to know
+ * Those who need to know are described in the TEST_FAIL_MAILS heroku config variable
  */
 function MailingReporter(tests_runner) {
 
@@ -14,7 +14,6 @@ function MailingReporter(tests_runner) {
 
     const recipients = (process.env.TEST_FAIL_MAILS || '').split(',').filter(email => email.length > 0)
 
-    mocha.reporters.Base.call(this, tests_runner);
     let passed = 0,
         failed = 0;
     //store info about failed tests here
@@ -46,17 +45,15 @@ function MailingReporter(tests_runner) {
         //there were failed tests and we have poeple to tell it about
         if (failed && recipients.length > 0 && process.env.SENDGRID_API_KEY) {
 
-            const send_grid = require('@sendgrid/mail');
-            send_grid.setApiKey(process.env.SENDGRID_API_KEY);
-
             const failure_mail = `
-                A recent push has <b>broken the build!<b>. Failing tests:
+                A recent push has <b>broken the build!</b>. <br/>
+                Failing tests:
                 <ul style="padding-left: 1em; list-style-type: circle; ">
-                    ${failed_tests.map(failed_obj => `<li style="padding: 0.5em 0em; line-height: 1.5">${failed_obj.title}<br/><span color="red">${failed_obj.error}<span></li>`)}
+                    ${failed_tests.map(failed_obj => `<li style="padding: 0.5em 0em; line-height: 1.5">${failed_obj.title}<br/><span style="color: red;">${failed_obj.error}<span></li>`).join('\n')}
                 </ul>
                 <br/>
                 <p>Total failed: ${failed}/${passed + failed}</p>
-                <p>Heroku will keep serving previous deploy until this is fixed. <br/>
+                <p>Heroku will keep serving the previous deploy until this is fixed. <br/>
                 Thank you in advance.
             `
 
@@ -67,16 +64,9 @@ function MailingReporter(tests_runner) {
                 html: failure_mail
             };
 
-            send_grid.sendMultiple(msg).then((result) => {
-                console.log(`Email '${MAIL_SUBJECT}' sent to ${recipients} OK: %o`, result);
-                process.exit(failed);
-            }).catch(err => {
-                console.error('EMAIL ERROR %o!', err);
-                process.exit(failed);
-            })
-
-
-            console.log(`Sent ${failed}/${passed + failed} fails email to ${recipients.length} recipients: ${recipients}`);
+            tests_runner.msg_object = msg;
+            
+            console.log(`Generated mail for ${failed}/${passed + failed} fails email to ${recipients.length} recipients: ${recipients}`);
         } else {
             //check conditions if failing to send
             if (failed) {
