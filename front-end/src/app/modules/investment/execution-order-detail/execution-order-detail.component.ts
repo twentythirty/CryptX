9ca3d530bd7081dad19b3ce8d89c38fce1f111c4
laurasud <laurasudeikyte@gmail.com>
2018-09-05 11:20:34 +0300

    execution order filter fix

--- front-end/src/app/modules/investment/execution-order-detail/execution-order-detail.component.ts
+++ front-end/src/app/modules/investment/execution-order-detail/execution-order-detail.component.ts
@@ -76,13 +76,13 @@ export class ExecutionOrderDetailComponent extends TimelineDetailComponent imple
     header: [
       { column: 'id', nameKey: 'table.header.id', filter: { type: 'number', hasRange: false, inputSearch: true, sortable: true }},
       { column: 'instrument', nameKey: 'table.header.instrument', filter: { type: 'text', sortable: true }},
-      { column: 'side', nameKey: 'table.header.side', filter: { type: 'text', sortable: true }},
+      { column: 'side', nameKey: 'table.header.side', filter: { type: 'text', sortable: true, inputSearch: false }},
       { column: 'exchange', nameKey: 'table.header.exchange', filter: { type: 'text', sortable: true }},
-      { column: 'type', nameKey: 'table.header.type', filter: { type: 'text', sortable: true }},
+      { column: 'type', nameKey: 'table.header.type', filter: { type: 'text', sortable: true, inputSearch: false }},
       { column: 'price', nameKey: 'table.header.price', filter: { type: 'number', sortable: true }},
       { column: 'total_quantity', nameKey: 'table.header.total_quantity', filter: { type: 'number', sortable: true }},
       { column: 'exchange_trading_fee', nameKey: 'table.header.exchange_trading_fee', filter: { type: 'number', sortable: true }},
-      { column: 'status', nameKey: 'table.header.status', filter: { type: 'text', sortable: true }},
+      { column: 'status', nameKey: 'table.header.status', filter: { type: 'text', sortable: true, inputSearch: false  }},
       { column: 'submission_time', nameKey: 'table.header.submission_time', filter: { type: 'date', sortable: true }},
       { column: 'completion_time', nameKey: 'table.header.completion_time', filter: { type: 'date', sortable: true }}
     ],
