0377608c80d87fdd3b11bd13616c174f3f1c9061
Tautvydas Petkunas <tautvydas@mediapark.com>
2018-09-28 14:49:58 +0300

    ask bid fetcher add try catch to handle exchange errors separately fix error when fetchTickers returned undefined catch and all job log errors to console

--- back-end/jobs/exchange-ask-bid-fetcher.js
+++ back-end/jobs/exchange-ask-bid-fetcher.js
@@ -41,92 +41,103 @@ module.exports.JOB_BODY = async (config, log) => {
         return Promise.all(
             _.map(exchanges_with_mappings, async (exchange) => {
 
-                let [err, con_result] = await to(Promise.all([
-                    ccxtUtils.getConnector(exchange.id),
-                    ccxtUtils.getThrottle(exchange.api_id)
-                ]));
-                if (err) {
-                    log(`Failed to get exchange connector for ${exchange.api_id}. Error: ${err.message}`);
-                    return [];
-                }
-                let [fetcher, throttle] = con_result;
-                if (!fetcher || fetcher.loading_failed) {
-                    
-                    log(`[ERROR.3A]: Failed to find connector for exchange ${exchange.name} or failed to load exchange markets!`);
-                    return "No healthy connector to fetch tickers with!";
-                }
+                let failed_instruments, fetched_market_data;
 
-                let mappings = associatedMappings[exchange.id];
-                let needed_ticker_data = [];
-                
-                if (fetcher.has.fetchTickers) { 
-                    // fetch instrument market data all at once
-                    log(`Fetching all market data from ${exchange.name}.`);
+                try {
                     
-                    let [err, all_ticker_data] = await to(fetcher.fetchTickers()); // fetch all instrument data
-
-                    if (err || _.isUndefined(all_ticker_data)) {
-                        if (err) log(`Failed to fetch market data from ${exchange.name}, error: `, err.message);
-                        else log(`Market data fetching request completed but response was empty for ${exchange.name}`);
+                    let [err, con_result] = await to(Promise.all([
+                        ccxtUtils.getConnector(exchange.id),
+                        ccxtUtils.getThrottle(exchange.api_id)
+                    ]));
+                    if (err) {
+                        log(`Failed to get exchange connector for ${exchange.api_id}. Error: ${err.message}`);
+                        return [];
                     }
+                    let [fetcher, throttle] = con_result;
+                    if (!fetcher || fetcher.loading_failed) {
+                        
+                        log(`[ERROR.3A]: Failed to find connector for exchange ${exchange.name} or failed to load exchange markets!`);
+                        return [];
+                    }
+
+                    let mappings = associatedMappings[exchange.id];
+                    let needed_ticker_data = [];
                     
-                    needed_ticker_data = _.map(mappings, mapping => { // check if received all data and return it
-                        if (!all_ticker_data.hasOwnProperty(mapping.external_instrument_id)) { // no instrument found in data
-                            log(`Market data from ${exchange.name} didn't include data for ${mapping.external_instrument_id}`);
-                            
-                            return [null, mapping];
+                    if (fetcher.has.fetchTickers) { 
+                        // fetch instrument market data all at once
+                        log(`Fetching all market data from ${exchange.name}.`);
+                        
+                        let [err, all_ticker_data] = await to(fetcher.fetchTickers()); // fetch all instrument data
+                        log(`Finished fetching all tickers from ${exchange.name}`);
+                        
+                        if (err || _.isUndefined(all_ticker_data)) {
+                            if (err) log(`Failed to fetch market data from ${exchange.name}, error: `, err.message);
+                            else log(`Market data fetching request completed but response was empty for ${exchange.name}`);
+
+                            return [];
                         }
                         
-                        return [ all_ticker_data[mapping.external_instrument_id], mapping ];
-                    });
-                } else {
-                    // fetch instrument market data one by one because exchange doesn't support fetching all at once
-                    needed_ticker_data = await Promise.all(
-                        _.map(mappings, async (mapping) => {
-                            log(`Fetching data from ${exchange.name} for symbol ${mapping.external_instrument_id}...`);
-
-                            let [err, ticker] = await to(throttle.throttled( // fetch single instrument (slowed down)
-                                Promise.resolve([]), fetcher.fetchTicker, mapping.external_instrument_id
-                            ));
-
-                            if (err) { // couldn't get single instrument
-                                log(`Failed to fetch ${mapping.external_instrument_id} instrument market data from ${exchange.name}`);
-
+                        needed_ticker_data = _.map(mappings, mapping => { // check if received all data and return it
+                            if (!all_ticker_data.hasOwnProperty(mapping.external_instrument_id)) { // no instrument found in data
+                                log(`Market data from ${exchange.name} didn't include data for ${mapping.external_instrument_id}`);
+                                
                                 return [null, mapping];
-                            }   
-
-                            return [ ticker, mapping ];
-                        })
-                    )
-                }
-
-                // partition failed and successfully received data
-                let [failed_instruments, fetched_market_data] = _.partition(needed_ticker_data, (market_data) => {
-                    let [ticker, mapping] = market_data;
-                    return !ticker || !_.isNumber(ticker.ask) || !_.isNumber(ticker.bid);
-                });
-
-                if (failed_instruments.length) {
-                    await Promise.all(_.map(failed_instruments, (failed_instrument) => {
-                        let [ticker, mapping] = failed_instrument;
-
-                        let failed_prices = [];
-                        if (!ticker) failed_prices.push('ask', 'bid'); 
-                        else {
-                            if (!_.isNumber(ticker.ask)) failed_prices.push('ask');
-                            if (!_.isNumber(ticker.bid)) failed_prices.push('bid');
-                        }
-
-                        return logAction(actions.instrument_without_data, { 
-                            args: {
-                                types: failed_prices,
-                                instruments: mapping.external_instrument_id,
-                                exchange: exchange.name,
-                            },
-                            relations: { exchange_id: exchange.id }
+                            }
+                            
+                            return [ all_ticker_data[mapping.external_instrument_id], mapping ];
                         });
-                    }));
-                } // check if the are failed to fetch instruments
+                    } else {
+                        // fetch instrument market data one by one because exchange doesn't support fetching all at once
+                        needed_ticker_data = await Promise.all(
+                            _.map(mappings, async (mapping) => {
+                                log(`Fetching data from ${exchange.name} for symbol ${mapping.external_instrument_id}...`);
+
+                                let [err, ticker] = await to(throttle.throttled( // fetch single instrument (slowed down)
+                                    Promise.resolve([]), fetcher.fetchTicker, mapping.external_instrument_id
+                                ));
+
+                                if (err) { // couldn't get single instrument
+                                    log(`Failed to fetch ${mapping.external_instrument_id} instrument market data from ${exchange.name}`);
+
+                                    return [null, mapping];
+                                }   
+
+                                return [ ticker, mapping ];
+                            })
+                        )
+                    }
+
+                    // partition failed and successfully received data
+                    [failed_instruments, fetched_market_data] = _.partition(needed_ticker_data, (market_data) => {
+                        let [ticker, mapping] = market_data;
+                        return !ticker || !_.isNumber(ticker.ask) || !_.isNumber(ticker.bid);
+                    });
+
+                    if (failed_instruments.length) {
+                        await Promise.all(_.map(failed_instruments, (failed_instrument) => {
+                            let [ticker, mapping] = failed_instrument;
+
+                            let failed_prices = [];
+                            if (!ticker) failed_prices.push('ask', 'bid'); 
+                            else {
+                                if (!_.isNumber(ticker.ask)) failed_prices.push('ask');
+                                if (!_.isNumber(ticker.bid)) failed_prices.push('bid');
+                            }
+
+                            return logAction(actions.instrument_without_data, { 
+                                args: {
+                                    types: failed_prices,
+                                    instruments: mapping.external_instrument_id,
+                                    exchange: exchange.name,
+                                },
+                                relations: { exchange_id: exchange.id }
+                            });
+                        }));
+                    } // check if the are failed to fetch instruments
+
+                } catch (error) {
+                    console.log(`Fetching and processing data from ${exchange.name} failed with error: ${error}`);
+                }
                 
                 return fetched_market_data;
             })
@@ -156,6 +167,7 @@ module.exports.JOB_BODY = async (config, log) => {
         else
             return "No records inserted!";
     }).catch(async err => {
+        log(`Job failed with error: ${err.message}`);
         await logAction(actions.job_error, {
             args: {
                 error: err.message
