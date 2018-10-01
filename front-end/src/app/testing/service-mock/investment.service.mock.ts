5c6b4bd1665c68e33d3848395e929ef156a5c3f8
laurasud <laurasudeikyte@gmail.com>
2018-10-01 09:30:26 +0300

    dashboard table testing

---
+++ front-end/src/app/testing/service-mock/investment.service.mock.ts
@@ -0,0 +1,19 @@
+export const getAllInvestmentsData = {
+  success: true,
+  investment_runs: [
+    {
+      completed_timestamp: null,
+      deposit_usd: '0',
+      id: 137,
+      is_simulated: 'investment.is_simulated.yes',
+      started_timestamp: 1538123482803,
+      status: 'investment.status.302',
+      strategy_type: 'investment.strategy.101',
+      updated_timestamp: 1538123491226,
+      user_created: 'Admin ',
+      user_created_id: 1,
+    }
+  ],
+  count: 1,
+  footer: []
+};
---
+++ front-end/src/app/testing/service-mock/modelConstants.service.ts
@@ -0,0 +1,4 @@
+export const getStrategiesData = [
+    ['MCI', 101],
+    ['LCI', 102]
+];
