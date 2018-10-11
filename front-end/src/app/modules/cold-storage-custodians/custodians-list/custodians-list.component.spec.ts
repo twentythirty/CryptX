a76a40fb40b9baf7d6b0b3ce377f873532dc4604
laurasud <laurasudeikyte@gmail.com>
2018-10-11 11:20:59 +0300

    cold storage custodian-list component tests

--- front-end/src/app/modules/cold-storage-custodians/custodians-list/custodians-list.component.spec.ts
+++ front-end/src/app/modules/cold-storage-custodians/custodians-list/custodians-list.component.spec.ts
@@ -1,31 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { extraTestingModules, fakeAsyncResponse } from '../../../testing/utils';
+import { extraTestingModules, fakeAsyncResponse, click } from '../../../testing/utils';
 
 import { CustodiansListComponent } from './custodians-list.component';
 import { ColdStorageCustodiansModule } from '../cold-storage-custodians.module';
-import { ColdStorageService, CustodiansAllResponse } from '../../../services/cold-storage/cold-storage.service';
-
-
-const ColdStorageServiceStub = {
-  getAllCustodians: () => {
-    return fakeAsyncResponse<CustodiansAllResponse>({
-      success: true,
-      custodians: [
-        {
-          id: 1,
-          name: 'Coinbase Custody'
-        }
-      ],
-      footer: [],
-      count: 1
-    });
-  }
-};
+import { ColdStorageService } from '../../../services/cold-storage/cold-storage.service';
+import { getAllCustodiansData } from '../../../testing/service-mock/coldStorage.service.mock';
 
 
 describe('CustodiansListComponent', () => {
   let component: CustodiansListComponent;
   let fixture: ComponentFixture<CustodiansListComponent>;
+  let coldStorageService: ColdStorageService;
+  let getAllCustodiansSpy;
+  let navigateSpy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -34,21 +21,38 @@ describe('CustodiansListComponent', () => {
         ...extraTestingModules
       ],
       providers: [
-        { provide: ColdStorageService, useValue: ColdStorageServiceStub }
+        ColdStorageService,
       ]
     }).compileComponents();
-    fixture = TestBed.createComponent(CustodiansListComponent);
-    fixture.detectChanges();
   }));
 
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CustodiansListComponent);
     component = fixture.componentInstance;
+    coldStorageService = fixture.debugElement.injector.get(ColdStorageService);
+    getAllCustodiansSpy = spyOn (coldStorageService, 'getAllCustodians').and.returnValue(fakeAsyncResponse(getAllCustodiansData));
+    navigateSpy = spyOn (component.router, 'navigate');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should correctly load table data on init', () => {
+    fixture.whenStable().then(() => {
+      expect(component.custodiansDataSource.body).toEqual(getAllCustodiansData.custodians);
+      expect(component.custodiansDataSource.footer).toEqual(getAllCustodiansData.footer);
+      expect(component.count).toEqual(getAllCustodiansData.count);
+    });
+  });
+
+  it('should navigate to add cusodian route on add cusodian button press', () => {
+    fixture.whenStable().then(() => {
+      const addCustodianButton = fixture.nativeElement.querySelector('a.start');
+      click(addCustodianButton);
+      expect(navigateSpy).toHaveBeenCalledWith(['cold_storage/custodians/add']);
+    });
   });
 });
