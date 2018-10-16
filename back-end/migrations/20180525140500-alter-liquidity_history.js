768df39193679a396cbc3e0aa41137d43bb32e39
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-10-16 16:46:11 +0300

    fix migration logic for fresh DB

--- back-end/migrations/20180525140500-alter-liquidity_history.js
+++ back-end/migrations/20180525140500-alter-liquidity_history.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const InstrumentLiquidityHistory = require('../models').InstrumentLiquidityHistory;
-
 module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.addColumn('instrument_liquidity_history', 'timestamp_from', {
@@ -10,16 +8,11 @@ module.exports = {
 
             return queryInterface.renameColumn('instrument_liquidity_history', 'date', 'timestamp_to');
         }).then(done => {
-
-            //update liquidity according to new dates (add 24h ago start)
-            return InstrumentLiquidityHistory.findAll();
-        }).then(histories => {
-
-            return Promise.all(_.map(_.filter(histories, 'timestamp_to'), history => {
-                
-                history.timestamp_from = new Date(history.timestamp_to.getTime() - (24 * 60 * 60 * 1000));
-                return history.save();
-            }));
+            //update any existing liquidities
+            return Promise.resolve(queryInterface.sequelize.query(`
+                UPDATE instrument_liquidity_history
+                SET timestamp_from = timestamp_to - INTERVAL '1 DAY'
+            `));
         });
     },
     down: (queryInterface, Sequelize) => {
