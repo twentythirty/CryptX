f7268cb73ffc61b6dbde5887915a18fbf69914d4
Vladislav Achramovic <v.achramovic@gmail.com>
2018-11-30 15:27:43 +0200

    Added HTTPS enforcment

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const compression = require('compression')
 const backend = require('./back-end/app.js');
+const enforce = require('express-sslify');
 
 const app = express();
+app.use(enforce.HTTPS({
+    trustXForwardedHostHeader: true,
+    trustProtoHeader: true
+}));
+
 app.use(compression()); // enables gzip to compress files
 
 // serve front-end files
